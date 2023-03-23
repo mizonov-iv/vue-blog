98f4553cb5f86299ea71a6ca55c22cc3a09a4c94
mizonov-iv <mizonov.iv@gmail.com>
2023-03-23 18:47:55 +0500

    first component

---
+++ src/components/Timeline.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs ">
+      <a href="">Today</a>
+      <a href="">This Week</a>
+      <a href="">This Month</a>
+    </span>
+  </nav>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
