55adf5beb46d3d0d89085bc651dbca4e7c7c579b
mizonov <mizonov.iv@gmail.com>
2023-09-29 14:44:38 +0500

    navbar update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,42 +1,55 @@
 <template>
   <nav>
     <a
-        href=""
-        :class="{'active': period === selectedPeriod}"
-        v-for="period in periods"
-        :key="period"
-        @click.prevent="selectPeriod(period)"
+       href=""
+       v-for="period in periods"
+       :key="period"
+       @click.prevent="selectPeriod(period)"
     >
       {{period}}
     </a>
   </nav>
-  <h1>{{selectedPeriod}}</h1>
+  <p>{{selectedPeriod}}</p>
   <ul>
     <li v-for="post in posts" :key="post">
       <p>{{post.title}}</p>
-      <p>{{post.created}}</p>
+      <p>{{post.created.toFormat("d MMM")}}</p>
     </li>
   </ul>
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import {Post, today, thisWeek, thisMonth} from "../posts";
+import {DateTime} from "luxon";
 
-const periods = ["today", "this week", "this month"] as const;
-
-type Period = typeof periods[number]
-
-const posts: Post[] = [
-    today,
-    thisWeek,
-    thisMonth
-]
-
-function selectPeriod (period: Period) {
-  selectedPeriod.value = period;
-}
+const periods = ["today", "this week", "this month"] as const
+type Period = typeof periods[number];
 
 const selectedPeriod = ref<Period>("today");
 
+const posts = computed( () => {
+  return [today, thisWeek, thisMonth]
+      .map(post => {
+        return {
+          ...post,
+          created: DateTime.fromISO(post.created)
+        }
+      })
+      .filter(post => {
+        if (selectedPeriod.value === "today") {
+          return post.created >= DateTime.now().minus({day: 1})
+        }
+
+        if (selectedPeriod.value === "this week") {
+          return post.created >= DateTime.now().minus({week: 1})
+        }
+
+        return post
+      })
+})
+
+const selectPeriod = (period: Period) => {
+    selectedPeriod.value = period
+  }
 </script>
